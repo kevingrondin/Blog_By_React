5603c03c7afbcfa00c9ace7dd6bfb4e6bb65186f
Kati Frantz <frantzenterprises777@gmail.com>
2018-04-08 01:22:49 +0100

    create SignUpForm component

---
+++ src/components/Signup/SignUpForm/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const SignUpForm = ({ handleInputChange, handleSubmit, errors }) => ((
+  <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
+    <div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
+      <h5 className="text-uppercase text-center">Register</h5>
+      <br />
+      <br />
+      <form className="form-type-material" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input type="text" name="name" onChange={handleInputChange} className="form-control" placeholder="Username" />
+          {
+            errors['name'] &&
+            <small className="text-danger">{errors['name']}</small>
+          }
+        </div>
+        <div className="form-group">
+          <input type="text" name="email" onChange={handleInputChange} className="form-control" placeholder="Email address" />
+          {
+            errors['email'] &&
+            <small className="text-danger">{errors['email']}</small>
+          }
+        </div>
+        <div className="form-group">
+          <input type="password" name="password" onChange={handleInputChange} className="form-control" placeholder="Password" />
+          {
+            errors['password'] &&
+            <small className="text-danger">{errors['password']}</small>
+          }
+        </div>
+        <div className="form-group">
+          <input type="password" name="password_confirmation" onChange={handleInputChange} className="form-control" placeholder="Password (confirm)" />
+        </div>
+        <br />
+        <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
+      </form>
+      <hr className="w-30" />
+      <p className="text-center text-muted fs-13 mt-20">Already have an account?
+      <a href="login.html">Sign in</a>
+      </p>
+    </div>
+  </div>
+));
+
+export default SignUpForm;
--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -4,6 +4,8 @@ import { validateAll } from 'indicative'
 
 import config from '../../config'
 
+import SignUpForm from './SignUpForm'
+
 class Signup extends React.Component {
   constructor() {
     super();
@@ -37,45 +39,11 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
-        <div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
-          <h5 className="text-uppercase text-center">Register</h5>
-          <br />
-          <br />
-          <form className="form-type-material" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <input type="text" name="name" onChange={this.handleInputChange} className="form-control" placeholder="Username" />
-              {
-                this.state.errors['name'] &&
-                <small className="text-danger">{this.state.errors['name']}</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="text" name="email" onChange={this.handleInputChange} className="form-control" placeholder="Email address" />
-              {
-                this.state.errors['email'] &&
-                <small className="text-danger">{this.state.errors['email']}</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="password" name="password" onChange={this.handleInputChange} className="form-control" placeholder="Password" />
-              {
-                this.state.errors['password'] &&
-                <small className="text-danger">{this.state.errors['password']}</small>
-              }
-            </div>
-            <div className="form-group">
-              <input type="password" name="password_confirmation" onChange={this.handleInputChange} className="form-control" placeholder="Password (confirm)" />
-            </div>
-            <br />
-            <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
-          </form>
-          <hr className="w-30" />
-          <p className="text-center text-muted fs-13 mt-20">Already have an account?
-      <a href="login.html">Sign in</a>
-          </p>
-        </div>
-      </div>
+      <SignUpForm
+        handleInputChange={this.handleInputChange}
+        handleSubmit={this.handleSubmit}
+        errors={this.state.errors}
+      />
     );
   }
 }
