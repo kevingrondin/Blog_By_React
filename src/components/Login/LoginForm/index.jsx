a6b0d87d2659186d657a022f39cb33dfa76cf46b
Kati Frantz <frantzenterprises777@gmail.com>
2018-04-25 02:00:22 +0100

    add container Login component add presentation LoginForm component

---
+++ src/components/Login/LoginForm/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Login = ({ handleInputChange, handleSubmit }) => ((
+  <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
+    <div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
+      <h5 className="text-uppercase text-center">Login</h5>
+      <br />
+      <br />
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            onChange={handleInputChange}
+            name="email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            onChange={handleInputChange}
+            name="password"
+          />
+        </div>
+        <div className="form-group flexbox py-10">
+          <label className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" defaultChecked />
+            <span className="custom-control-indicator" />
+            <span className="custom-control-description">Remember me</span>
+          </label>
+          <a className="text-muted hover-primary fs-13" href="#">Forgot password?</a>
+        </div>
+        <div className="form-group">
+          <button className="btn btn-bold btn-block btn-primary" type="submit">Login</button>
+        </div>
+      </form>
+      <hr className="w-30" />
+      <p className="text-center text-muted fs-13 mt-20">Dont have an account?
+        <a href="register.html">Sign up</a>
+      </p>
+    </div>
+  </div>
+));
+
+Login.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default Login;
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,36 +1,34 @@
 import React from 'react';
 
-const Login = () => ((
-  <div className="mh-fullscreen bg-img center-vh p-20" style={{ backgroundImage: 'url(assets/img/bg-girl.jpg)' }}>
-    <div className="card card-shadowed p-50 w-400 mb-0" style={{ maxWidth: '100%' }}>
-      <h5 className="text-uppercase text-center">Login</h5>
-      <br />
-      <br />
-      <form>
-        <div className="form-group">
-          <input type="text" className="form-control" placeholder="Username" />
-        </div>
-        <div className="form-group">
-          <input type="password" className="form-control" placeholder="Password" />
-        </div>
-        <div className="form-group flexbox py-10">
-          <label className="custom-control custom-checkbox">
-            <input type="checkbox" className="custom-control-input" defaultChecked />
-            <span className="custom-control-indicator" />
-            <span className="custom-control-description">Remember me</span>
-          </label>
-          <a className="text-muted hover-primary fs-13" href="#">Forgot password?</a>
-        </div>
-        <div className="form-group">
-          <button className="btn btn-bold btn-block btn-primary" type="submit">Login</button>
-        </div>
-      </form>
-      <hr className="w-30" />
-      <p className="text-center text-muted fs-13 mt-20">Dont have an account?
-        <a href="register.html">Sign up</a>
-      </p>
-    </div>
-  </div>
-));
+import LoginForm from './LoginForm';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (<LoginForm
+      handleInputChange={this.handleInputChange}
+      handleSubmit={this.handleSubmit}
+    />);
+  }
+}
 
 export default Login;
