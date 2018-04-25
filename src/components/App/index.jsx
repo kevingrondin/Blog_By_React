468f4976c90990bba4f5623e72eee2a457b8c100
Kati Frantz <frantzenterprises777@gmail.com>
2018-04-25 01:23:01 +0100

    fix eslint errors in index.js file install babel-eslint parser for class properties

---
+++ src/components/App/index.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
+
+import Login from '../Login';
+import Navbar from '../Navbar';
+import Signup from '../Signup';
+import Footer from '../Footer';
+import Welcome from '../Welcome';
+import CreateArticle from '../CreateArticle';
+import SingleArticle from '../SingleArticle';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  componentWillMount() {
+    const user = localStorage.getItem('user');
+
+    if (user) {
+      this.setState({
+        authUser: JSON.parse(user),
+      });
+    }
+  }
+
+  setAuthUser = (authUser) => {
+    this.setState({
+      authUser,
+    });
+  }
+
+  render() {
+    const { location } = this.props;
+    return (
+      <div>
+        {
+          location.pathname !== '/login' && location.pathname !== '/signup' &&
+          <Navbar authUser={this.state.authUser} />
+        }
+        <Route exact path="/" component={Welcome} />
+        <Route path="/login" component={Login} />
+        <Route
+          path="/signup"
+          render={
+            props => (<Signup
+              {...props}
+              registerUser={this.props.authService.registerUser}
+              setAuthUser={this.setAuthUser}
+            />)
+            }
+        />
+        <Route path="/article/:slug" component={SingleArticle} />
+        <Route path="/articles/create" component={CreateArticle} />
+        {
+          location.pathname !== '/login' && location.pathname !== '/signup' &&
+          <Footer />
+        }
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  authService: PropTypes.objectOf(PropTypes.func).isRequired,
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,81 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, withRouter } from 'react-router-dom';
+import { BrowserRouter, withRouter } from 'react-router-dom';
 
-import Login from './components/Login'
-import Navbar from './components/Navbar';
-import Signup from './components/Signup';
-import Footer from './components/Footer';
-import Welcome from './components/Welcome';
-import SingleArticle from './components/SingleArticle';
-import CreateArticle from './components/CreateArticle';
+import App from './components/App';
 
 import AuthService from './services/auth';
 
 import registerServiceWorker from './registerServiceWorker';
 
-class App extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      authUser: null
-    };
-  }
-
-  componentDidMount() {
-    const user = localStorage.getItem('user')
-
-    if (user) {
-      this.setState({
-        authUser: JSON.parse(user)
-      })
-    } 
-  }
-
-  setAuthUser = (authUser) => {
-    this.setState({
-      authUser
-    });
-  }
-
-  render() {
-    const { location } = this.props;
-    return (
-      <div>
-        {
-          location.pathname !== '/login' && location.pathname !== '/signup' &&
-          <Navbar authUser={this.state.authUser} />
-        }
-        <Route exact path="/" component={Welcome} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" render={
-            (props) => <Signup {...props}
-                registerUser={this.props.authService.registerUser}
-                setAuthUser={this.setAuthUser} />
-            } />
-        <Route path="/article/:slug" component={SingleArticle} />
-        <Route path="/articles/create" component={CreateArticle} />
-        {
-          location.pathname !== '/login' && location.pathname !== '/signup' &&
-          <Footer />
-        }
-      </div>
-    );
-  }
-}
-
-
-const Main = withRouter((props) => {
-  return (
-    <App authService={new AuthService()} {...props} />
-  );
-});
+const Main = withRouter(props => ((
+  <App authService={new AuthService()} {...props} />
+)));
 
 ReactDOM.render(
   <BrowserRouter>
     <Main />
   </BrowserRouter>
-  , document.getElementById('root'));
+  , document.getElementById('root'),
+);
 registerServiceWorker();
